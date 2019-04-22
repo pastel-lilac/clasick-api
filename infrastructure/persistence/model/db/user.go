e16b6fa24475e76cf91d9b2edb1b9989f0556bbb
tozastation <b1016031@fun.ac.jp>
2019-04-22 21:04:19 +0900

    [Fix] NVARCHAR Constraint

--- infrastructure/persistence/model/db/user.go
+++ infrastructure/persistence/model/db/user.go
@@ -3,10 +3,10 @@ package db
 type User struct {
 	BaseModel
 	Name         string `gorm:"not null"`
-	Password     string `gorm:"not null"`
+	Password     string `gorm:"not null;type:varchar(500)"`
 	Email        string
 	PhoneNum     string
-	AccessToken  string `gorm:"not null"`
+	AccessToken  string `gorm:"not null;type:varchar(1000)"`
 	IconPath     string
 	Introduction string
 }
